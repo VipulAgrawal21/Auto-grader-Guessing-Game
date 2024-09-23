456143d31079832315be665f9170210e3c703952
VipulAgrawal21 <169054960+VipulAgrawal21@users.noreply.github.com>
2024-09-23 15:37:11 +0530

    Update guess.php

--- guess.php
+++ guess.php
@@ -12,9 +12,9 @@
     echo("Your guess is too short");
   } else if ( ! is_numeric($_GET['guess']) ) {
     echo("Your guess is not a number");
-  } else if ( $_GET['guess'] < 79 ) {
+  } else if ( $_GET['guess'] < 30 ) {
     echo("Your guess is too low");
-  } else if ( $_GET['guess'] > 79 ) {
+  } else if ( $_GET['guess'] > 30 ) {
     echo("Your guess is too high");
   } else {
     echo("Congratulations - You are right");
